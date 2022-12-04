5191d8c352fcdb62ecdcb83a47c542a4c37020c0
HK-TO <ituki.8.18.haruka.9.11@outlook.jp>
2022-12-04 11:50:46 +0900

    fix test directory

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,6 @@ class TestTensorboardIntoMatplotlib(unittest.TestCase):
 
     def test_open(self):
 
-        path =  "../files/asymmetric_ff_asac_InvertedPendulum-v4"# @param {typee:"string"}
-        output_json_file_name = '../outputs/test.json' #@param {type:"string"}
+        path =  "tests/files/asymmetric_ff_asac_InvertedPendulum-v4"# @param {typee:"string"}
+        output_json_file_name = 'tests/outputs/test.json' #@param {type:"string"}
         tensorboard_to_json(path, output_json_file_name)
