940051fa894603dd91a757a49f4d7e53daf42e30
HK-TO <ituki.8.18.haruka.9.11@outlook.jp>
2022-12-06 15:43:03 +0900

    add some new tests

--- tensorboard_into_matplotlib/main.py
+++ tensorboard_into_matplotlib/main.py
@@ -1,7 +1,6 @@
 import os
 import json
 
-
 import tensorboard as tb
 import matplotlib.pyplot as plt
 
@@ -22,24 +21,3 @@ def tensorboard_to_json(input_path, output_path, output_file_name):
     with open(os.path.join(output_path, output_file_name), 'w') as fout:
         json.dump(scalars, fout)
 
-
-#@title 複数のTensorBoardからそれぞれのJsonfileに変換する関数
-# import glob
-# def TensorboardtoJson(path, output_json_name):
-#   event_acc = EventAccumulator(path, size_guidance={'scalars': 0})
-#   event_acc.Reload() # ログファイルのサイズによっては非常に時間がかかる
-#   #すべてのscalarデータを取得しそのjsonファイルを取得する
-#   scalars = {}
-#   for tag in event_acc.Tags()['scalars']:
-#       events = event_acc.Scalars(tag)
-#       scalars[tag] = [event.value for event in events]
-#   with open(output_json_name, 'w') as fout:
-#       json.dump(scalars, fout)
-
-
-# input_file_names = "/content/events.*" #@param {type: "string"}
-# output_file_name = "/content/arc_CVI_float2_ANNep" #@param {type: "string"}
-# files = glob.glob(input_file_names)
-# for i in range(len(files)):
-#   output_json_file_name = output_file_name + str(i+1) +'.json'
-#   TensorboardtoJson(files[i], output_json_file_name)
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,41 @@
 import unittest
+
+import json
+import glob
+
 from tensorboard_into_matplotlib.main import tensorboard_to_json
 
 class TestTensorboardIntoMatplotlib(unittest.TestCase):
 
     def test_open(self):
 
-        input_path =  "tests/files/asymmetric_ff_asac_InvertedPendulum-v4"# @param {typee:"string"}
+        input_file_name =  "tests/content/asymmetric_ff_asac_InvertedPendulum-v4/events.out.tfevents.1668517881.takumao-System-Product-Name.3781.0"# @param {typee:"string"}
         output_path =  "tests/outputs"# @param {typee:"string"}
         output_file_name = 'test.json' #@param {type:"string"}
-        tensorboard_to_json(input_path, output_path, output_file_name)
+        tensorboard_to_json(input_file_name, output_path, output_file_name)
+
+    def test_open_multiple(self):
+
+        input_file_names = "tests/content/asymmetric_ff_asac_InvertedPendulum-v4/events.*" #@param {type: "string"}
+        output_path =  "tests/outputs"# @param {typee:"string"}
+        output_file_name = 'multiple_test' #@param {type:"string"}
+
+        files = glob.glob(input_file_names)
+        for i in range(len(files)):
+          output_json_file_name = output_file_name + str(i+1) +'.json'
+          tensorboard_to_json(files[i], output_path, output_json_file_name)
+
+    def test_confirm_key(self):
+        json_name = "tests/outputs/multiple_test1.json"
+        expected = {'reward/train': None, 
+                    'reward/test': None, 
+                    'loss/critic_1': None,
+                    'loss/critic_2': None, 
+                    'loss/entropy_loss': None, 
+                    'entropy_temprature/alpha': None}
+
+        with open(json_name) as f:
+            jsn = json.load(f)
+
+        self.assertEqual(jsn.keys(), expected.keys())
+
